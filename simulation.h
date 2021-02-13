b74e273e769b49476670d03b07a2267994f52a91
Bhupen <bhupend.dabholkar96@gmail.com>
2021-02-13 21:33:15 +0530

    simulation variables

---
+++ simulation.h
@@ -0,0 +1,31 @@
+#ifndef SIMULATION_H
+#define SIMULATION_H
+#include<vector>
+#include "operator.h"
+
+using namespace std; 
+
+class Simulation{
+	friend class Observables;
+
+	struct bond{int i; int j;};
+
+	int Lx, Ly, n_op, L, N, Nb;
+	float beta, probfactor;
+	const float a=1.0/3.0;
+	int *spin,*vfirst,*vlast;
+	bond *bondlist;
+	vector<Operator> opstring;
+
+	public:
+	void initparameters(int, int, float);
+	void make_lattice();
+	void initspinstate();
+	void diagonalupdate();
+	void make_vertexlist();
+	void set_loop(int,int);
+	void make_loops();
+	void find_truncation();
+};
+
+#endif
