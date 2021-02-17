4d7a7f7e4b184777dd23ba4a0f0ef0c2d18b1cc3
Bhupen <bhupend.dabholkar96@gmail.com>
2021-02-17 14:03:50 +0530

    energy and error binning functions added

--- observables.cpp
+++ observables.cpp
@@ -1,4 +1,6 @@
 #include<iostream>
+#include<vector>
+#include<math.h>
 #include "observables.h"
 
 using namespace std;
@@ -15,3 +17,32 @@ void Observables :: get_truncation(Simulation obj){cout << obj.L << "\n";}
 void Observables :: get_diagcount(Simulation obj){cout << obj.n_op << "\n";}
 
 void Observables :: get_temperature(Simulation obj){cout << 1.0/obj.beta << "\n";}
+
+void Observables :: init_energy(Simulation obj){
+	E_binavg=0.0;
+	Ebins.reserve(obj.nbins);
+}
+
+void Observables :: updt_Ebinavg(Simulation obj){E_binavg += obj.n_op;}
+
+void Observables :: collect_Ebinavg(Simulation obj){
+	E_binavg=E_binavg/((float) obj.mcs);
+	E_binavg=E_binavg/(obj.beta*obj.N) - 0.5;
+	Ebins.push_back(E_binavg);
+	E_binavg=0.0;
+}
+
+
+float Observables :: get_energy(Simulation obj){
+	float sum=0.0;
+	for(int bin=0; bin<obj.nbins; bin++){sum+=Ebins[bin];}
+	E_avg=sum/obj.nbins;
+	return sum/obj.nbins;
+}
+
+float Observables :: get_energy_error(Simulation obj){
+	float error=0.0;
+	for(int bin=0; bin<obj.nbins; bin++){error += powf((Ebins[bin] - E_avg),2.0);}
+	error=error/(obj.nbins*(obj.nbins-1));
+	return powf(error,0.5);
+}
